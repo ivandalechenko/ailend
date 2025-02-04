ab2f76cab571d834f8ff478cea2cc7d89e1b1d8e
ivandalechenko <ivandalechenko@gmail.com>
2025-02-04 05:41:16 +0200

    fix

--- src/Media.jsx
+++ src/Media.jsx
@@ -1,7 +1,7 @@
 export default () => {
     return (
         <>
-            <a href='#' className='Header_link'>
+            <a href='https://dexscreener.com/solana/8vgkqclsi6p6zgdcgdpmy2ukm4c4z7l8te2j545tkjae' className='Header_link'>
                 <img src="/img/dex.svg" alt="" />
             </a>
             <a href='https://x.com/xenovate' target="_blank" className='Header_link'>
